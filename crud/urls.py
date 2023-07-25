07d80827824382b26060631a526b0ad4c05e1828
rukeshshrestha <rukesh.shrestha11@gmail.com>
2023-07-25 09:00:48 +0545

    CRUD done templates rendering incomplete

--- crud/urls.py
+++ crud/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 
-from .views import index,about,create,contacts,partData
+from .views import index,about,create,contacts,partData,deleteBlog,updateBlog
 
 urlpatterns = [
     path("",index,name="home"),
     path("about/",about),
     path("create/",create),
-    path("<int:id>/",partData),
-    path("contacts/",contacts)
+    path("<int:id>/",partData,name="particular"),
+    path("contacts/",contacts),
+    path("delete/<int:id>/",deleteBlog,name="deleteblog"),
+    path("update/<int:id>/",updateBlog,name="updateblog")
+
 
    
     
--- crud/views.py
+++ crud/views.py
@@ -7,7 +7,7 @@ from .forms import BlogForm
 # Create your views here.
 def index(request):
     blog = Blog.objects.all()
-    return render(request,"crud/index.html",{"blogs":blog})
+    return render(request,"crud/home.html",{"blogs":blog})
 
 
 def about(request):
@@ -19,6 +19,20 @@ def create(request):
         form.save()
         return  redirect("home")
     return render(request,"crud/create.html",{"form":form})
+
+def deleteBlog(request,id):
+    blog = Blog.objects.get(id=id)
+    blog.delete()
+    return redirect("home")
+
+def updateBlog(request,id):
+    blog = Blog.objects.get(id=id)
+    form = BlogForm(request.POST or None,instance=blog)
+    if (form.is_valid()):
+        form.save()
+        return redirect("home")
+    return render(request,"crud/create.html",{"form":form})
+    
 
 def partData(request,id):
     blog = Blog.objects.get(id=id)
@@ -39,9 +53,8 @@ def contacts(request):
         )
 
         contacts.save()
+        return redirect("home")
       
-        
-    
-    
+           
 
     return render(request,"crud/contactus.html")
