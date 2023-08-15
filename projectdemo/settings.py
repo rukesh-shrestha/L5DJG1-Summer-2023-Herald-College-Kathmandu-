0e9981501c3a12d40fd2de15b19112eb77286215
rukeshshrestha <rukesh.shrestha11@gmail.com>
2023-08-15 09:26:39 +0545

    file added

--- projectdemo/settings.py
+++ projectdemo/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://blogapp-d4d9.onrender.com','blog.rukesh-shrestha.com.np']
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
